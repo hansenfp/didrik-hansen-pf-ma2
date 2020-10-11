716d9c286517a70f3254b66fc827f7ad6f322bc6
hansenfp <61291503+hansenfp@users.noreply.github.com>
2020-10-11 19:33:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log (petArray[1]);
+
+// question 3
+petArray.push("sheep");
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject = {
+    name: "rasnys",
+    colour: "brown",
+    age: 26
+};
+
+// question 6
+console.log(catObject[2]);
+
+// question 7
+var catArray = [
+    {
+    name: "rasnys",
+    colour: "brown",
+    age: 26
+    }
+];
+
+// question 8
+console.log(catArray);
+for(var i=0; i < catArray.length; i++) {
+    console.log(i);
+}
+// question 9
+function logToConsole(name) {
+    console.log(name);
+}
+
+// question 10
+console.log("donkey");
